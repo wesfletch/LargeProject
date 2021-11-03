5b0cfd92c8d57a295d9c19ae2d5c463326123ec1
KingJMS1 <1kingjms@gmail.com>
2021-11-02 20:36:42 -0400

    Added registration and password checking

--- Mobile/lib/pages.dart
+++ Mobile/lib/pages.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'inlay_pages.dart';
+import 'package:fltr_test/utilities.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage(this.submitUsername, this.submitPassword, this.doLogin, {Key? key}) : super(key: key);
@@ -59,20 +60,178 @@ class LoginPage extends StatelessWidget {
               ),
               Padding(
                 padding: const EdgeInsets.all(10),
-                child: SizedBox(
-                  width: 400,
-                  height: 40,
-                  child:
-                    ElevatedButton(
-                      onPressed: () {doLogin();},
-                      child: const Text("Sign In", style: TextStyle(fontSize: 24)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(
+                      width: 400,
+                      height: 40,
+                      child: ElevatedButton(
+                        onPressed: () {doLogin();},
+                        child: const Text("Sign In", style: TextStyle(fontSize: 24)),
+                      ),
                     ),
+                    SizedBox(
+                      width: 400,
+                      height: 40,
+                      child: ElevatedButton(
+                        onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => const RegistrationPage()));},
+                        child: const Text("Register", style: TextStyle(fontSize: 24)),
+                        style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Theme.of(context).scaffoldBackgroundColor), side: MaterialStateProperty.all(const BorderSide(width: 3.0, color: Colors.purple))),
+                      ),
+                    ),
+                  ],
                 )
               )
             ],
           ),
         ]),
       ),
+    );
+  }
+}
+
+class RegistrationPage extends StatefulWidget  {
+  const RegistrationPage({Key? key}): super(key: key);
+  @override State<StatefulWidget> createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage>  {
+  String? _password;
+  String? _username;
+  String? _otherPassword;
+  List<passwordErrors> _passwordErrors = isValidPassword("");
+  String _registrationError = "";
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Stack(children: [
+          Column(
+              children: [Center(child: Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 40, 10, 10),
+                  child: RichText(
+                      text: TextSpan(
+                          text: "<appname>",
+                          style: Theme.of(context).textTheme.headline2))))]),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Register",
+                      style: Theme.of(context).textTheme.headline1,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  onChanged: (String? a) {_username = a;},
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                    hintText: "123@gmail.com",
+                  ),
+                  autofocus: true,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                    children: [
+                      Row(children: const [Text("Password Requirements:")]),
+                      Padding(child: Column(
+                        children: [
+                          Row(children: [Icon(_passwordErrors.contains(passwordErrors.tooShort)? Icons.clear : Icons.check, color: _passwordErrors.contains(passwordErrors.tooShort)? Colors.red : Colors.green), const Text("Password length is 8 characters or more")]),
+                          Row(children: [Icon(_passwordErrors.contains(passwordErrors.noUpper)? Icons.clear : Icons.check, color: _passwordErrors.contains(passwordErrors.noUpper)? Colors.red : Colors.green), const Text("Password contains at least 1 uppercase character")]),
+                          Row(children: [Icon(_passwordErrors.contains(passwordErrors.noLower)? Icons.clear : Icons.check, color: _passwordErrors.contains(passwordErrors.noLower)? Colors.red : Colors.green), const Text("Password contains at least 1 lowercase character")]),
+                          Row(children: [Icon(_passwordErrors.contains(passwordErrors.noNumber)? Icons.clear : Icons.check, color: _passwordErrors.contains(passwordErrors.noNumber)? Colors.red : Colors.green), const Text("Password contains at least 1 number")]),
+                          Row(children: [Icon(_passwordErrors.contains(passwordErrors.noSpecial)? Icons.clear : Icons.check, color: _passwordErrors.contains(passwordErrors.noSpecial)? Colors.red : Colors.green), const Text("Password contains at least 1 special character")]),
+                          Row(children: [Icon(stringsMatch(_password, _otherPassword)? Icons.check : Icons.clear, color: stringsMatch(_password, _otherPassword) ? Colors.green : Colors.red), const Text("Passwords match")]),
+                        ],
+                      ), padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),)
+                    ]
+                )
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: TextField(
+                  onChanged: (String? a) {setState(() {
+                    _password = a;
+                    _passwordErrors = isValidPassword(a);
+                  });},
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: "Password",
+                    hintText: "Enter password here",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: TextField(
+                  onChanged: (String? a) {setState(() {
+                    _otherPassword = a;
+                  });},
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: "Enter password again",
+                    hintText: "Enter password again",
+                  ),
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(
+                        width: 400,
+                        height: 40,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_passwordErrors.isEmpty && stringsMatch(_password, _otherPassword)) {
+                              // TODO: Register
+                              Navigator.pop(context);
+                              Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => const RegistrationSuccessfulPage()));
+                            }
+                            else  {
+                              setState(() {
+                                _registrationError = "Error: Please ensure your password meets all password requirements";
+                              });
+                            }
+                          },
+                          child: const Text("Register", style: TextStyle(fontSize: 24)),
+                        ),
+                      ),
+                    ],
+                  )
+              ),
+              _registrationError.isNotEmpty ? Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [const Icon(Icons.clear, color: Colors.red,), Text(_registrationError)],),
+              ) : const Text(""),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class RegistrationSuccessfulPage extends StatelessWidget  {
+  const RegistrationSuccessfulPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Padding(padding: EdgeInsets.all(10), child: RichText(text: TextSpan(text: "Registration successful. Please watch for an email to confirm your email address.", style: Theme.of(context).textTheme.headline1), textAlign: TextAlign.center,))),
     );
   }
 }
