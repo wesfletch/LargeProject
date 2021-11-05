6d18c0adb85392b88b95459d865d9f93436af180
Gregory Travis <gregorytravis711@gmail.com>
2021-11-05 19:08:29 -0400

    Intial Webclient Commit

---
+++ Webclient/frontend/src/components/Login.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function Login()
+{
+    return (
+        <div class="row">
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
+            <div class="col-lg-6 col-md-8 col-sm-10">
+                <div class="signIn">
+                    <h1>Sign In</h1>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
+        </div>
+    );
+};
+
+export default Login;
