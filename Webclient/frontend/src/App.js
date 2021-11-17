49e481f3cb545873b8464141c1815e4fadb39654
Gregory Travis <gregorytravis711@gmail.com>
2021-11-17 00:12:08 -0500

    Updated login page and added figma design

--- Webclient/frontend/src/App.js
+++ Webclient/frontend/src/App.js
@@ -4,11 +4,6 @@ import LoginPage from './pages/LoginPage';
 import './App.css';
 
 function App() {
-  /*return (
-    <div style={{backgroundImage: 'url("https://lh3.googleusercontent.com/SLVY3pZlxA5fC48C3rKG4NSA2W00O__aPsq8TOz6gHH1MHQYK5vilWcUruIyVOsQM93baKydERZ6IM4O_VscwO0LcvlGGa5qJc8t7PmmQeCBl8f7NJc9Yvzw2e86k6ViOYloV0yc=w2400")'}}>
-     <LoginPage />
-    </div>
-  );*/
   return (
     <div class="main">
      <LoginPage />
--- Webclient/frontend/src/components/Login.js
+++ Webclient/frontend/src/components/Login.js
@@ -3,32 +3,50 @@ import {Form, Button} from 'react-bootstrap';
 
 function Login()
 {
+    var email;
+    var password;
+
+    const doLogin = async event =>
+    {
+        event.preventDefault();
+        alert("test" + email.value + password.value);
+    };
+
     return (
         <div class="row">
             <div class="col-lg-3 col-md-2 col-sm-1"></div>
             <div class="col-lg-6 col-md-8 col-sm-10">
                 <div class="mainForms">
-                    <div class="signIn">
-                        <h1>Sign In</h1>
-                    </div>
+                    <h1 class="signIn">Sign In</h1>
                     <div class="forms">
                         <Form>
                             <Form.Group class="formElement">
                                 <Form.Label>Username</Form.Label>
-                                <Form.Control type="email" placeholder="123@gmail.com"/>
+                                <Form.Control type="email" placeholder="123@gmail.com" ref={(c) => email = c}/>
                             </Form.Group>
                         </Form>
                         <Form>
                             <Form.Group class="formElement">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Enter password here"/>
+                                <Form.Control type="password" placeholder="Enter password here" ref={(c) => password = c}/>
                             </Form.Group>
                         </Form>
                     </div>
                     <div class="signIn">
-                        <Button variant='signInBtn' size="big">
+                        <Button variant='signInBtn' size="big" onClick={doLogin}>
                             Sign In
                         </Button>
+                    </div>
+                    <div class="noAccount">
+                        <Form.Label>Don't have an account?</Form.Label>
+                    </div>
+                    <div class="guest">
+                        <Button variant='guestBtn' size="bigG">
+                            Continue as guest
+                        </Button>
+                    </div>
+                    <div class="links">
+                        <a>Create account</a>
                     </div>
                 </div>
             </div>
