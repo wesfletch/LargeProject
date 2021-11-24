6b5d7f0c8b35059a9f100f37db74dab87532914e
Wesley Fletcher <wesfletch@gmail.com>
2021-11-24 12:29:21 -0500

    rebasing Webclient

---
+++ Webclient/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginPage from './pages/LoginPage';
+import './App.css';
+
+function App() {
+  return (
+    <div class="main">
+     <LoginPage />
+    </div>
+  );
+}
+
+export default App;
