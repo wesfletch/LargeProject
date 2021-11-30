0f69bb96e8559571d0b6682ae12e0bc651119dad
ChelseaOshodi <92893913+ChelseaOshodi@users.noreply.github.com>
2021-11-30 17:13:58 -0500

    Update User.js

--- API_Things/routes/User.js
+++ API_Things/routes/User.js
@@ -35,19 +35,19 @@ userRouter.post('/register',(req,res)=>{
             return res.status(400).json({message : {msgBody : "Error: Email is already taken.", msgError: "N/A"}});
         }//Checks if email is valid
         if(!Validator.isEmail(email)){
-            return res.status(400).json({message : {msgBody : "Error: Invalid email.", msgError: "N/A"}});
+            return res.status(401).json({message : {msgBody : "Error: Invalid email.", msgError: "N/A"}});
         }
         //Checks if password is a valid length
         if(!Validator.isLength(password, { min: 6, max: 30 })){
-            return res.status(400).json({message : 
+            return res.status(402).json({message : 
                 {msgBody : "Error: Password must be between 6 and 30 characters.", msgError: "N/A"}});
         }
         //Confirms that both passwords match
         if(!Validator.equals(password,password2)){
-            return res.status(400).json({message : {msgBody : "Error: Passwords must match.", msgError: "N/A"}});
+            return res.status(403).json({message : {msgBody : "Error: Passwords must match.", msgError: "N/A"}});
         }
         if (!email || !password || !name) {
-            return res.status(400).json({message : {msgBody : "Error: Please provide an email, name, and password.", msgError: "N/A"}});
+            return res.status(405).json({message : {msgBody : "Error: Please provide an email, name, and password.", msgError: "N/A"}});
         }
         else{   //Saves the new user
             const newUser = new User({
@@ -76,7 +76,7 @@ userRouter.post('/register',(req,res)=>{
 
                        return res.status(200).json({message : {msgBody : "User successfully saved.", msgError: false}});
                     }catch (err) {
-                        return res.status(500).json({message : {msgBody : "Error: User saved. Email could not be sent.", msgError: true}});
+                        return res.status(501).json({message : {msgBody : "Error: User saved. Email could not be sent.", msgError: true}});
                     }
                 }    
             });
@@ -164,7 +164,7 @@ userRouter.post('/forgot', async(req, res) => {
         res.status(500).json({message : {msgBody : "Error sending email.", msgError: err}});
       }
     } catch (err) {
-      return res.status(500).json({message : {msgBody : "An Error Occured.", msgError: err}});
+      return res.status(501).json({message : {msgBody : "An Error Occured.", msgError: err}});
     }
 });
 
@@ -187,7 +187,7 @@ userRouter.put('/reset/:resetToken', async(req, res) => {
     }
     //Confirms that both passwords match
     if(!Validator.equals(req.body.password,req.body.password2)){
-        return res.status(400).json({message : {msgBody : "Error: Passwords must match.", msgError: "N/A"}});
+        return res.status(401).json({message : {msgBody : "Error: Passwords must match.", msgError: "N/A"}});
     }
 
     user.password = req.body.password;
@@ -214,18 +214,18 @@ userRouter.post('/add/', passport.authenticate('jwt',{session : false}),async(re
             return res.status(500).json({message : {msgBody : "Error finding friend.", msgError: err}});
         }
         if(!doc){ //Checks if email is in database
-            return res.status(400).json({message : {msgBody : "Error: User not found.", msgError: "N/A"}});
+            return res.status(404).json({message : {msgBody : "Error: User not found.", msgError: "N/A"}});
         }
         if(doc){ //Checks if friend already exists in friend's list
             if(req.user.friends.indexOf(doc.id.toString()) === -1) {
                 User.findByIdAndUpdate({_id:req.user._id},{$push:{friends : doc._id}})
                 .then(() =>User.findByIdAndUpdate(doc._id,{$push:{friends : req.user.id}}))
                 .then(() => res.status(200).json({message : {msgBody : "Successfully  added friend.", msgError: "N/A"}}))
-                .catch(err => res.status(500).json({message : {msgBody : "Error adding friend.", msgError: err}}));
+                .catch(err => res.status(501).json({message : {msgBody : "Error adding friend.", msgError: err}}));
             }
             else{
                 //Else friend already exists in friend's list
-                return res.status(400).json({message : {msgBody : "Error: User is already your friend.", msgError: "N/A"}});
+                return res.status(401).json({message : {msgBody : "Error: User is already your friend.", msgError: "N/A"}});
             }
         }
     });
@@ -264,7 +264,7 @@ userRouter.post('/addplaylist', authorized ,(req,res)=>{
             req.user.playlists.push(playlist);
             req.user.save(err=>{
                 if(err)
-                    res.status(500).json({message : {msgBody : "Error saving playlist.", msgError: err}});
+                    res.status(501).json({message : {msgBody : "Error saving playlist.", msgError: err}});
                 else
                 res.status(200).json({message : {msgBody : "Successfully created paylist.", msgError: "N/a"}});
             });
