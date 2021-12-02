c2ce7a5bc2b00cb413feb6141a18b81df7988bae
Gregory Travis <gregorytravis711@gmail.com>
2021-12-02 11:58:25 -0500

    Fixing routing error

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ if (process.env.NODE_ENV === 'production')
     app.use(express.static('Webclient/frontend/build'));
     app.get('*', (req, res) =>
     {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'Webclient', 'frontend', 'build', 'index.html'));
     });
 }
