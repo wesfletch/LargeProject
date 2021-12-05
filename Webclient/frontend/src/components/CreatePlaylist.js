f9508c07c01b07f0bd49e4229ff63696ef2ad070
Gregory Travis <gregorytravis711@gmail.com>
2021-12-04 21:07:40 -0500

    Added playlist table

--- Webclient/frontend/src/components/CreatePlaylist.js
+++ Webclient/frontend/src/components/CreatePlaylist.js
@@ -4,7 +4,17 @@ import {Form, Button} from 'react-bootstrap';
 
 function CreatePlaylist()
 {
-    const path = 'https://poosd-f2021-11.herokuapp.com/users/addplaylist';
+    function getPath()
+    {
+        if (process.env.NODE_ENV === 'production')
+        {
+            return 'https://poosd-f2021-11.herokuapp.com/users/addplaylist';
+        }
+        else
+        {
+            return 'http://localhost:5000/users/addplaylist';
+        }
+    }
     var name;
     const doCreatePlaylist = async event =>
     {
@@ -13,7 +23,7 @@ function CreatePlaylist()
         var js = JSON.stringify(obj);
         try
         {
-            const response = await fetch(path, {method:'POST',credentials: 'same-origin', body:js,headers:{'Content-Type':'application/json'}});
+            const response = await fetch(getPath(), {method:'POST',credentials: 'include', body:js,headers:{'Content-Type':'application/json'}});
             var res = JSON.parse(await response.text());
             //alert(res);
             window.location.href = '/home';
--- Webclient/frontend/src/components/Login.js
+++ Webclient/frontend/src/components/Login.js
@@ -4,7 +4,17 @@ import {Form, Button} from 'react-bootstrap';
 
 function Login()
 {
-    const path = 'https://poosd-f2021-11.herokuapp.com/users/login';
+    function getPath()
+    {
+        if (process.env.NODE_ENV === 'production')
+        {
+            return 'https://poosd-f2021-11.herokuapp.com/users/login';
+        }
+        else
+        {
+            return 'http://localhost:5000/users/login';
+        }
+    }
     var email;
     var password;
 
@@ -16,7 +26,7 @@ function Login()
         var js = JSON.stringify(obj);
         try
         {
-            const response = await fetch(path, {method:'POST',credentials: 'same-origin',body:js,headers:{'Content-Type':'application/json'}});
+            const response = await fetch(getPath(), {method:'POST',credentials: 'include', body:js,headers:{'Content-Type':'application/json'}});
             var res = JSON.parse(await response.text());
             if (res.isAuthenticated)
             {
